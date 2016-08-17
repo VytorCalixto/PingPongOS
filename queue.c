baad62452119739407641f3ae14d3780ac9b9e3f
Vytor Calixto <vytorcalixto@gmail.com>
2016-08-17 17:57:44 -0300

    First commit
    P0: Queue library

---
+++ queue.c
@@ -0,0 +1,22 @@
+/**
+ * Program: queue.c
+ * Author: Vytor Calixto
+ */
+#include "queue.h"
+
+void queue_append(queue_t **queue, queue_t *elem) {
+    return;
+}
+
+queue_t *queue_remove(queue_t **queue, queue_t *elem) {
+    return elem;
+}
+
+int queue_size(queue_t *queue) {
+    int size = 0;
+    return size;
+}
+
+void queue_print(char *name, queue_t *queue, void print_elem(void*)) {
+    return;
+}
